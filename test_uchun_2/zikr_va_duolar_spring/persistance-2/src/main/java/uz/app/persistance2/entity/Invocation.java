df46db8cfcfc75164c7099a7a83b7f29711b2e29
mardonesanov <mardonesanov21@gmail.com>
2024-11-20 11:08:52 +0500

    zikr va duolar

--- test_uchun_2/zikr_va_duolar_spring/persistance-2/src/main/java/uz/app/persistance2/entity/Invocation.java
+++
@@ -1,9 +0,0 @@
-package uz.app.persistance2.entity;
-
-public interface Invocation {
-    Long getId();
-    String getName();
-    String getArabic();
-    String getTransliteration();
-    String getTranslation();
-}
